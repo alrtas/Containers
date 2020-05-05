152c0fd4ac3f2c6b0fdb2d0137a74d1207a3e4bf
Thiago Alberto da Silva <32065208+alrtas@users.noreply.github.com>
2020-05-05 11:56:10 -0300

    test blip chat

--- PHP/Files/index - Copia.php
+++ PHP/Files/index - Copia.php
@@ -1,3 +1,18 @@
-<?php 
-    phpinfo();
-?>
+<script>
+    (function () {
+        window.onload = function () {
+          var blipClient = new BlipChat()
+          .withAppKey('Y2FsbGNlbnRlcmludGVsYnJhc2hvbW9sb2cyOjVhNGE4NDE4LTkxNTItNDEyYi05MWMxLWFjY2UyNmFmY2E3Yg==')
+          .withButton({"color":"#474747","icon":""})
+          .withEventHandler(BlipChat.LOAD_ACCOUNT_EVENT, function () {
+              blipClient.sendMessage({
+                  "type": "application/vnd.lime.chatstate+json",
+                  "content": {
+                    "state": "starting"
+                  }
+              });
+          });
+          blipClient.build();
+        }
+    })();
+</script>
