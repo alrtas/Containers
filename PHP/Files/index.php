b512a587dd447fc1daf667ebdb5d50cc8629c608
Thiago Alberto da Silva <32065208+alrtas@users.noreply.github.com>
2020-05-05 11:52:39 -0300

    Testing BLIP chat

--- PHP/Files/index.php
+++ PHP/Files/index.php
@@ -1,34 +1,19 @@
 <?php 
 
-    require_once 'HTTP/Request2.php';
-    $request = new HTTP_Request2();
-    $request->setUrl('https://msging.net/commands');
-    $request->setMethod(HTTP_Request2::METHOD_POST);
-    $request->setConfig(array(
-      'follow_redirects' => TRUE
-    ));
-    $request->setHeader(array(
-      'Content-Type' => 'application/json',
-      'Authorization' => 'Key YXRlbmRlbnRlZGlnaXRhbG9wY29lczpmZkVmVm5vUGJVS3ZESk5nMDk0OQ=='
-    ));
-    $request->setBody('{  
-    \n  "id": "e63f4b51-3327-41db-97f3-596e1f72d0e7",
-    \n  "method": "get",
-    \n  "uri": "/contacts/7b8c8ed8-bdac-40cf-8016-2874f30c5158@tunnel.msging.net"
-    \n}');
-    try {
-      $response = $request->send();
-      if ($response->getStatus() == 200) {
-        echo $response->getBody();
-      }
-      else {
-        echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
-        $response->getReasonPhrase();
-      }
-    }
-    catch(HTTP_Request2_Exception $e) {
-      echo 'Error: ' . $e->getMessage();
-    }
-    
 ?>
-
+<html>
+<body>
+    <script src="https://unpkg.com/blip-chat-widget" type="text/javascript"></script>
+<script>
+    (function () {
+        window.onload = function () {
+            new BlipChat()
+            .withAppKey('Y2FsbGNlbnRlcmludGVsYnJhc2hvbW9sb2cyOjVhNGE4NDE4LTkxNTItNDEyYi05MWMxLWFjY2UyNmFmY2E3Yg==')
+            .withButton({"color":"#2CC3D5","icon":""})
+            .withCustomCommonUrl('https://chat.blip.ai')
+            .build();
+        }
+    })();
+</script>
+</body>
+</html>
