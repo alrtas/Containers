288a1e6b89bb371ebdbb97515553d9bb1a18642a
Thiago Alberto da Silva <32065208+alrtas@users.noreply.github.com>
2020-05-05 12:06:34 -0300

    Update index1.php

--- PHP/Files/index1.php
+++ PHP/Files/index1.php
@@ -1,11 +1,11 @@
 <script src="https://unpkg.com/blip-chat-widget" type="text/javascript"></script>
 <script>
-    (function(){
-        window.onload = function() {
-            var blipClient = new BlipChat()
+    (function () {
+        window.onload = function () {
+          var blipClient = new BlipChat()
           .withAppKey('Y2FsbGNlbnRlcmludGVsYnJhc2hvbW9sb2cyOjVhNGE4NDE4LTkxNTItNDEyYi05MWMxLWFjY2UyNmFmY2E3Yg==')
           .withButton({"color":"#474747","icon":""})
-          .withEventHandler(BlipChat.LOAD_ACCOUNT_EVENT, function () {
+          .withEventHandler(BlipChat.CREATE_ACCOUNT_EVENT, function () {
               blipClient.sendMessage({
                   "type": "application/vnd.lime.chatstate+json",
                   "content": {
@@ -17,3 +17,4 @@
         }
     })();
 </script>
+
