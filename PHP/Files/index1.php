6993f34c98160d9356716f02f328652982d24e33
Thiago Alberto da Silva <32065208+alrtas@users.noreply.github.com>
2020-05-07 11:59:12 -0300

    Update index1.php

--- PHP/Files/index1.php
+++ PHP/Files/index1.php
@@ -1,8 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    
+        <div class="col-md-4 col-sm-5 mx-3 my-3" >
+                <div class="card bg-white text-center border-dark">
+                    <div class="card-body" id="chat">
+                    </div>
+                    </div>
+                </div>
+        </div>  
+      
+      
+      
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+      
+      
+      
+      
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+
 <script src="https://unpkg.com/blip-chat-widget" type="text/javascript"></script>
 <script>
     (function () {
         window.onload = function () {
           var blipClient = new BlipChat()
+          .withTarget("chat")
+          .withCustomStyle("div { height: 100; }")
           .withAppKey('Y2FsbGNlbnRlcmludGVsYnJhczo4NjY2YmVlNS04Y2FkLTQ3NTgtODc4NS1jNDNmZGJjNTQ4NzM=')
           .withButton({"color":"#474747","icon":""})
           .withEventHandler(BlipChat.CREATE_ACCOUNT_EVENT, function () {
