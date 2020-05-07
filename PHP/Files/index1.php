a27865a007f9a5dd7f0c93658f26357bf0fa25ff
Thiago Alberto da Silva <32065208+alrtas@users.noreply.github.com>
2020-05-07 12:00:32 -0300

    Update index1.php

--- PHP/Files/index1.php
+++ PHP/Files/index1.php
@@ -13,7 +13,6 @@
         <div class="col-md-4 col-sm-5 mx-3 my-3" >
                 <div class="card bg-white text-center border-dark">
                     <div class="card-body" id="chat"></div>
-                    </div>
                 </div>
         </div>  
       
