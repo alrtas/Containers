c39115d048e8c862a5f2cc804ac13de31abca52a
Thiago Alberto da Silva <32065208+alrtas@users.noreply.github.com>
2020-05-07 15:36:24 -0300

    Update index1.php

--- PHP/Files/index1.php
+++ PHP/Files/index1.php
@@ -43,7 +43,7 @@
         window.onload = function () {
           var blipClient = new BlipChat()
           .withTarget("chat")
-          .withCustomStyle("div { height: 1000; }")
+          .withCustomStyle("div { height: 1000px; }")
           .withAppKey('Y2FsbGNlbnRlcmludGVsYnJhczo4NjY2YmVlNS04Y2FkLTQ3NTgtODc4NS1jNDNmZGJjNTQ4NzM=')
           .withButton({"color":"#474747","icon":""})
           .withEventHandler(BlipChat.CREATE_ACCOUNT_EVENT, function () {
