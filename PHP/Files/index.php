7cd38660ed27ad158023435c395fbf1d747a7fb4
Thiago Alberto da Silva <alrrtas@gmail.com>
2021-03-01 10:16:28 -0300

    Update index.php

--- PHP/Files/index.php
+++ PHP/Files/index.php
@@ -6,26 +6,40 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <!-- Scrollbar Custom CSS -->
     
     <link rel="icon" type="image/png" href="https://desk.blip.ai/favicon.png"/>
     <title>BLiP HSM</title>
+
     <style>
 @import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
+
+
 p {
-    font-family: 'Ben', sans-serif;
+    font-family: 'Poppins', sans-serif;
     font-size: 1.1em;
     font-weight: 300;
     line-height: 1.7em;
     color: #999;
 }
+h5
+{
+    font-family: 'Poppins', sans-serif;
+    font-size: 2 em;
+    font-weight: 500;
+    line-height: 2em;
+    color: #999;
+}
+
 a, a:hover, a:focus {
     color: inherit;
     text-decoration: none;
     transition: all 0.3s;
 }
+
 #sidebar {
     /* don't forget to add all the previously mentioned styles here too */
     background: #242B36;
@@ -36,18 +50,22 @@ a, a:hover, a:focus {
     min-height: 100vh;
     text-align: center;
 }
+
 #sidebar .sidebar-header {
     padding: 20px;
     background: #242B36;
 }
+
 #sidebar ul.components {
     padding: 20px 0;
     border-bottom: 1px solid #242B36;
 }
+
 #sidebar ul p {
     color: #fff;
     padding: 10px;
 }
+
 #sidebar ul li a {
     padding: 10px;
     font-size: 1.1em;
@@ -58,11 +76,12 @@ a, a:hover, a:focus {
     color: #242B36;
     background: #fff;
 }
+
     </style>
   </head>
-  <body background-color = "#F7FAFB">
+  <body style = "background-color: #F7FAFB">
         <div class="row">
-            <div class="col-md-2">
+            <div class="col-md-2 col-sm-1">
                 <div class="wrapper">
                     <!-- Sidebar -->
                     <nav id="sidebar">
@@ -76,10 +95,10 @@ a, a:hover, a:focus {
                             <h4><b>Operações e Suporte</b></h4>
                             <br><br> <br><br>
                             <li>
-                                <a href="#">Disparo de mensagens</a>
+                                <a href="index.html">Disparo de mensagens</a>
                             </li>
                             <li>
-                                <a href="#">Cadastros de parceiros</a>
+                                <a href="./parceiros.html">Cadastros de parceiros</a>
                             </li>
                             <li>
                                 <a href="https://desk.blip.ai/">BLiP Desk</a>
@@ -92,40 +111,61 @@ a, a:hover, a:focus {
                 
                 </div>
             </div>
-            <div class="col-md-10">   
+            <div class="col-md-10 mt-5" >   
                 <div class="row justify-content-center mt-5">
-                    <div class="col-md-2">
-                    </div>
-                    <div class="col-md-8 mt-5">
+            
+                    <div class="col-md-8 col-lg-6 mt-5">
                         <div class="card text-center">
-                            <div class="card-header">
-                              <ul class="nav nav-pills card-header-pills">
-                                <li class="nav-item">
-                                  <a class="nav-link active" href="#">Active</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link" href="#">Link</a>
-                                </li>
-                                <li class="nav-item">
-                                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                                </li>
-                              </ul>
+                            <div class="card-header" style="background-color: #2e3745">
+                                <h5 class="card-title text-white">Começar uma nova conversa no WhatsApp</h5>
                             </div>
                             <div class="card-body">
-                              <h5 class="card-title">Aqui vai o disparo de HSM</h5>
-                              <p class="card-text">Abaixo terão campos de contato e templates</p>
-                              <a href="#" class="btn btn-primary">Enviar</a>
+                                <form>
+                                    <div class="form-group row mt-5 pt-5">
+                                        <label for="colFormLabel" class="col-sm-2 col-form-label"><p class="font-weight-light"> Parceiros:</p> </label>
+                                        <div class="col-md-4 col-sm-8">
+                                            <select id="inputState" class="form-control">
+                                                <option selected>Escolha o parceiro</option>
+                                                <option>parceiro 1</option>
+                                                <option>parceiro 2</option>
+                                                <option>parceiro 3</option>
+                                                <option>parceiro 4</option>
+                                            </select>
+                                        </div>
+                                            <label for="colFormLabel" class="col-sm-2 col-form-label"> <p class="font-weight-light"> Templates: </p> </label>
+                                            <div class="col-md-4 col-sm-8 ">
+                                                <select id="inputState" class="form-control">
+                                                    <option selected >Templates </option>
+                                                    <option>Cotação</option>
+                                                    <option>Status</option>
+                                                    <option>Cobrança</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row mt-2">
+                                            <label for="colFormLabel" class="col-sm-2 col-form-label"> <p class="font-weight-light">  Váriavel 1: </p></label>
+                                            <div class="col-sm-4"> <input type="text" class="form-control" placeholder="{{1}}"> </div>
+                                            <label for="colFormLabel" class="col-sm-2 col-form-label"> <p class="font-weight-light">  Váriavel 2: </p></label>
+                                            <div class="col-sm-4"> <input type="text" class="form-control" placeholder="{{2}}"> </div>
+                                        </div>
+                                        <div class="form-group row justify-content-center my-5 pt-5">   
+                                            <div class="col-7">
+                                                <button type="submit" class="btn btn-outline-success w-100">Enviar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </form>
                             </div>
                           </div>
-                          
                     </div>
-                    <div class="col-md-2">
-                        
-                    </div>
+            
                 </div>               
             </div>
         </div>
      
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>  
     <script>
         $(document).ready(function () {
@@ -134,27 +174,5 @@ a, a:hover, a:focus {
             });
         });
     </script>
-  <script src="https://unpkg.com/blip-chat-widget" type="text/javascript">
-</script>
-<script>
-    (function () {
-        window.onload = function () {
-            var blipClient = new BlipChat()
-            .withAppKey('b3BlcmFjb2VzZXN1cG9ydGVob21vbG9nOmVkYzAyNmU1LTY5ZDItNGQ5Ni1iNTBjLWQyMjUyNjQ1Yzk1Yw==')
-            .withButton({"color":"#00A75D","icon":"https://s3-sa-east-1.amazonaws.com/msging.net/iris/Media_6997bb92-d476-4151-b00c-811ee394d637"})
-            .withCustomCommonUrl('https://chat.blip.ai/')
-            .withEventHandler(BlipChat.CREATE_ACCOUNT_EVENT, function () {
-              blipClient.sendMessage({
-                  "type": "application/vnd.lime.chatstate+json",
-                  "content": {
-                    "state": "openshift",
-                    "channel" : "openShift"
-                  }
-              });
-          });
-          blipClient.build();
-        }
-    })();
-</script>
   </body>
 </html>
